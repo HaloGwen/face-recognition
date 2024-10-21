b77b6e894fedc41bc1f03f69db7e917cedd07b41
116180543+HaloGwen@users.noreply.github.com <116180543+HaloGwen@users.noreply.github.com>
2024-10-22 00:01:39 +0700

    1

--- train.py
+++ train.py
@@ -1,30 +1,55 @@
-import os
 import cv2
 import numpy as np
 from PIL import Image
-
-# Create an instance of LBPHFaceRecognizer
-recognizer = cv2.face.LBPHFaceRecognizer_create()
-path = "dataset"
-
-def get_images_with_id(path):
-    images_path = [os.path.join(path, f) for f in os.listdir(path)]
-    faces = []
-    ids = []
-
-    for single_image_path in images_path:
-        faceImg = Image.open(single_image_path).convert('L')
-        faceNp = np.array(faceImg, np.uint8)
-        id = int(os.path.split(single_image_path)[-1].split(".")[1])
-        print(id)
-        faces.append(faceNp)
-        ids.append(id)
-        cv2.imshow("Training", faceNp)
-        cv2.waitKey(10)
-
-    return np.array(ids), faces
-
-ids, faces = get_images_with_id(path)
-recognizer.train(faces, ids)
-recognizer.save("recognizer/trainingdata.yml")
-cv2.destroyAllWindows()
+data = [] 
+label = []
+for j in range (1,6):
+  for i in range (1,21):
+    filename = './dataset/anh'+ str(j) + '.'  + str(i) + '.jpg'
+    Img = cv2.imread(filename)
+    Img = cv2.cvtColor(Img, cv2.COLOR_BGR2GRAY)
+    Img = cv2.resize(src=Img, dsize=(100,100))
+    Img = np.array(Img)
+    data.append(Img)
+    label.append(j-1)
+data1 = np.array(data)
+label = np.array(label)
+data1 = data1.reshape((100,100,100,1))
+X_train = data1/255
+from sklearn.preprocessing import LabelBinarizer
+lb = LabelBinarizer()
+trainY =lb.fit_transform(label)
+from tensorflow.keras.models import Model
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import AveragePooling2D
+from tensorflow.keras.layers import MaxPooling2D
+from tensorflow.keras.layers import Conv2D
+from tensorflow.keras.layers import Activation
+from tensorflow.keras.layers import Flatten
+from tensorflow.keras.layers import Input
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import concatenate
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.optimizers import SGD
+Model = Sequential()
+shape = (100,100, 1)
+Model.add(Conv2D(32,(3,3),padding="same",input_shape=shape))
+Model.add(Activation("relu"))
+Model.add(Conv2D(32,(3,3), padding="same"))
+Model.add(Activation("relu"))
+Model.add(MaxPooling2D(pool_size=(2,2)))
+Model.add(Conv2D(64,(3,3), padding="same"))
+Model.add(Activation("relu"))
+Model.add(MaxPooling2D(pool_size=(2,2)))
+Model.add(Flatten())
+Model.add(Dense(512))
+Model.add(Activation("relu"))
+Model.add(Dense(5))
+Model.add(Activation("softmax"))
+Model.summary()
+Model.compile(loss='categorical_crossentropy',
+              optimizer='adam',
+              metrics=['accuracy'])
+print("start training")
+Model.fit(X_train,trainY,batch_size=5,epochs=10)
+Model.save("khuonmat.h5")
